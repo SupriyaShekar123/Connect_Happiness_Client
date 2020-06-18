3747aac56df8a3fdca3db9ba3ca3a5ca487f5671
SupriyaShekar123 <s.supriya82@gmail.com>
2020-06-18 11:11:31 +0200

    Added pages and components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React from "react";
+import { Switch, Route, NavLink } from "react-router-dom";
 
 import "./App.css";
 import "./components/Home";
 import "./components/OurServices";
-import "./components/Volunteer";
+
 import Home from "./components/Home";
+import OurServices from "./components/OurServices";
 
 function App() {
   return (
     <div className='App'>
-      <Home />
+      <nav>
+        <NavLink
+          activeStyle={{
+            fontWeight: "bold",
+            color: "red",
+          }}
+          to='/'>
+          Home
+        </NavLink>
+        <NavLink
+          activeStyle={{
+            fontWeight: "bold",
+            color: "green",
+          }}
+          to='/ourservices'>
+          OurServices
+        </NavLink>
+        <NavLink
+          activeStyle={{
+            fontWeight: "bold",
+            color: "blue",
+          }}
+          to='/login'>
+          Login
+        </NavLink>
+      </nav>
+      <Switch>
+        <Route exact path='/' component={Home} />
+
+        {/* <Route path='/signup' component={SignUp} />
+        <Route path='/login' component={Login} /> */}
+        <Route path='/ourservices' component={OurServices} />
+      </Switch>
     </div>
   );
 }
--- src/components/Volunteer.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Volunteer() {
-  return <div>Form for volunteers</div>;
-}
