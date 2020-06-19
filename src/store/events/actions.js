3d726a2ee4b6d52b5333df6e725ffa473cf0588f
SupriyaShekar123 <s.supriya82@gmail.com>
2020-06-19 10:01:00 +0200

    redux store for events done

---
+++ src/store/events/actions.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export async function getEventsLists(dispatch, getstate) {
+  dispatch({ type: "ARTLISTS_LOADING" });
+  try {
+    const response = await axios.get("http://localhost:4000/events");
+    console.log("THE RESPONSE ", response.data);
+    dispatch({ type: "EVENTS_LOADED", payload: response.data });
+  } catch (error) {
+    // console.log(" ERROR MSG : ", error.message)
+    dispatch({ type: "EVENTS_ERROR" });
+  }
+}
---
+++ src/store/events/reducer.js
@@ -0,0 +1,33 @@
+const initialState = {
+  loading: true,
+  events: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "EVENTS_LOADING": {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+
+    case "EVENTS_LOADED": {
+      //  console.log("ARTLIST DETAILS, action.payload);
+      return {
+        ...state,
+
+        loading: true,
+        lists: action.payload,
+      };
+    }
+    case "EVENTS_ERROR": {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+    default:
+      return state;
+  }
+};
